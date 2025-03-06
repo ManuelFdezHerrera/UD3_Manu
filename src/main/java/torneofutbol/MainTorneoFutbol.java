44645c94352da7a2d3bf5f06edfc1fbeb2585b95
ManuelFdezHerrera <mfernandezh14@educantabria.es>
2025-03-06 11:29:50 +0100

    MENSAJE DE LOCAL

--- src/main/java/torneofutbol/MainTorneoFutbol.java
+++ src/main/java/torneofutbol/MainTorneoFutbol.java
@@ -5,7 +5,7 @@ public class MainTorneoFutbol {
 
     public static void main(String[] args) {
 
-        System.out.println("Accediendo a la base de datos del torneo de FP");
+        System.out.println("MENSAJE DE LOCAL");
         
         // Crear una instancia de la clase Equipo
         Equipo equipoMH = new Equipo("IES MH FC");
